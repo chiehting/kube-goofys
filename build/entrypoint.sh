45cf3e1ce81d78ee8b6df293b775e1b8334a28ae
chiehting <ting911111@gmail.com>
2022-11-18 12:30:57 +0800

    feat: Allow other user can read/write files.

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-LOG_DIR="/var/log"
 MOUNT_POINT="${MOUNT_POINT:-/mnt}"
+LOG_DIR="${MOUNT_POINT}/log"
 AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
 AWS_S3_BUCKETS_NAME="$AWS_S3_BUCKETS_NAME"
@@ -20,12 +20,16 @@ fi
 
 
 echo "starting..."
-for bucket in $AWS_S3_BUCKETS_NAME;
+mkdir -p $LOG_DIR
+IFS=','
+for bucketName in $AWS_S3_BUCKETS_NAME;
 do
+  bucket=${bucketName// /}
   echo "mount $bucket to $MOUNT_POINT/$bucket"
-  mkdir -p $MOUNT_POINT/$bucket $LOG_DIR
+  mkdir -p $MOUNT_POINT/$bucket
   touch $LOG_DIR/$bucket.log
-  goofys --use-content-type --debug_fuse -f $bucket $MOUNT_POINT/$bucket >> $LOG_DIR/$bucket.log 2>&1 &
+  # The command goofys have an argument --debug_fuse, It's can enable fuse-related debugging output.
+  goofys -o allow_other --use-content-type --dir-mode=0777 --debug_s3 -f $bucket $MOUNT_POINT/$bucket >> $LOG_DIR/$bucket.log 2>&1 &
 done
 
 tail -f $LOG_DIR/*.log
--- build/stop.sh
+++ build/stop.sh
@@ -7,8 +7,10 @@ MOUNT_POINT="${MOUNT_POINT:-/mnt}"
 AWS_S3_BUCKETS_NAME="$AWS_S3_BUCKETS_NAME"
 
 echo "starting..."
-for bucket in $AWS_S3_BUCKETS_NAME;
+IFS=','
+for bucketName in $AWS_S3_BUCKETS_NAME;
 do
+  bucket=${bucketName// /}
   echo "umount $bucket to $MOUNT_POINT/$bucket"
   umount $MOUNT_POINT/$bucket
 done
