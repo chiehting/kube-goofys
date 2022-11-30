d8f4fcc14dfd00bb661c218511edc9095ade2446
chiehting <ting911111@gmail.com>
2022-11-30 15:14:36 +0800

    feat: Umount the folders before mount the folders.

--- build/stop.sh
+++ build/stop.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-set -e
-
-LOG_DIR="/var/log/goofys"
 MOUNT_POINT="${MOUNT_POINT:-/mnt}"
 AWS_S3_BUCKETS_NAME="$AWS_S3_BUCKETS_NAME"
 
-echo "starting..."
+echo "stoping..."
 IFS=','
 for bucketName in $AWS_S3_BUCKETS_NAME;
 do
   bucket=${bucketName// /}
-  echo "umount $bucket to $MOUNT_POINT/$bucket"
-  umount $MOUNT_POINT/$bucket
+  umount -rlf $MOUNT_POINT/$bucket || /bin/true
+  echo "umount $MOUNT_POINT/$bucket"
 done
 
